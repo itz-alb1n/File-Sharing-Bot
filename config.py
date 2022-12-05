5f0c4026913b73fcc118ca74267472ee5cddb323
itz-alb1n <115361467+itz-alb1n@users.noreply.github.com>
2022-12-05 19:54:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,13 +19,13 @@ APP_ID = int(os.environ.get("APP_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001595515906"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 #Port
-PORT = os.environ.get("PORT", "8080")
+PORT = os.environ.get("PORT", "8000")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hey {mention} !\n\n<i>I'm An Advanced File Sharing  bot Created For @MoviejunctionGrp 🔥</b></i>")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,10 +46,10 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>➡️ Please Join Our Backup Group 👉 @MovieJunctionGrp3 and Click Try Again Button Below to get The Movie You Requested.!\n\n➡️ നമ്മുടെ Backup Group-ൽ ജോയിൻ ചെയ്താൽ മാത്രമേ ഈ ബോട്ടിൽ നിന്ന് മൂവി കിട്ടു,അത് കൊണ്ട് മൂവി കിട്ടാൻ 👉 @MovieJunctionGrp3 Group-ൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള Try Again ബട്ടണിൽ ക്ലിക്ക് ചെയ്യുക. 🔐</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "{previouscaption}\n\n<b><i>♻️ JOIN : @MoviejunctionGrp 🔥</i></b>")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
@@ -61,10 +61,10 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "" ## "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+##ADMINS.append(1250450587)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
