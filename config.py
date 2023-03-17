76131888f0e4de75e95f2f3d828affdea0fdfe8c
itz-alb1n <115361467+itz-alb1n@users.noreply.github.com>
2023-03-17 18:45:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "❌<b><u> Action Not Permitted.!</u> \n\nDon't send me messages directly I'm only File Share bot!\n\nTo Request Series, Join @MoviejunctionGrp.</b>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
